24e1cf894479954c4320b4a6168c9977a7d2016d
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-07-29 22:55:54 -0300

    feat: Global application style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/home.module.scss";
+import "../styles/global.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
-import styles from "@/styles/home.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
       <Head>
-        <title>ig.news</title>
+        <title>Inicio | ig.news</title>
       </Head>
-      Ola Mundo
-    </h1>
+      <h1>Hello World</h1>
+    </>
   );
 }
