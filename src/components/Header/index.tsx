70e6fc1799ee133b4d39ba60fbae3738eb5d2ad8
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-07-30 22:38:29 -0300

    Feat header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignIn } from "../SignIn";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignIn />
+      </div>
+    </header>
+  );
+}
