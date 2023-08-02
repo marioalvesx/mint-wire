84f19b6896424e946fa360080db57901369b4986
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-02 02:16:01 -0300

    Feat: Finalize basic header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 
+import styles from "./home.module.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -10,8 +12,19 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <main>
-        <section></section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
