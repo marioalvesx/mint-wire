19c6e38cbd133859146b0052a3b5179ee3535eef
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-12 23:59:08 -0300

    wip: consuming stripe api

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,3 +1,10 @@
 import Stripe from "stripe";
+import { version } from "../../package.json";
 
-export const stripe;
+export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: "2023-08-11",
+  appInfo: {
+    name: "Ignews",
+    version,
+  },
+});
