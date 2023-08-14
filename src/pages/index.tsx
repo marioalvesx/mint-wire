6e755110ddf6d6888680af46c1de53284d0648f8
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-13 23:04:17 -0300

    feat: static site generation in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { Inter } from "next/font/google";
 
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1NbDIvBgJHfGwrf6Se8JeuPQ");
 
   const product = {
@@ -56,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
