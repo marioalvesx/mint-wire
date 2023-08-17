f36d760ec52935c11952774857dea8a2b238eb50
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-17 01:34:47 -0300

    feat: api routes

---
+++ src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: "Mario" },
+    { id: 2, name: "Duda" },
+    { id: 3, name: "Rafa" },
+  ];
+
+  return response.json(users);
+};
