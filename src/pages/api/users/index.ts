cfc3882d1603a119796079a06feffcb4ad72fbe5
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-18 23:48:08 -0300

    feat: adding parametrization

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
   const users = [
     { id: 1, name: "Mario" },
     { id: 2, name: "Duda" },
