41cafe5bb168780302d7a527599a93c57589288d
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-08-27 22:26:42 -0300

    feat: saving logged users in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
@@ -9,5 +13,23 @@ export const authOptions = {
       authorization: { params: { scope: "read:user" } },
     }),
   ],
-};
-export default NextAuth(authOptions);
+
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const isAllowedToSignIn = true;
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      } catch (error) {
+        return false;
+      }
+
+      return isAllowedToSignIn ? true : false;
+    },
+  },
+});
