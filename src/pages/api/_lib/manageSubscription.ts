3e82e98c2a5e6b5116b8f6230050a5bcde9a0c6e
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-09-25 23:59:19 -0300

    feat: save event data

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,7 @@
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+) {
+  // Busca usuário no banco do FaunaDB com o ID {customerId}
+  // Salvar os dados da subscription no FaunaDB
+}
