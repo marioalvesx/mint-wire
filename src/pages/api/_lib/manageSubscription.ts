ffc88aa17a9bd0d3e5835df8f18b9c9d016fe38e
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-09-26 23:56:37 -0300

    fix: save stripe subscription

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,7 +1,28 @@
+import { query as q } from "faunadb";
+import { fauna } from "@/services/fauna";
+import { stripe } from "@/services/stripe";
+
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string
 ) {
-  // Busca usuário no banco do FaunaDB com o ID {customerId}
-  // Salvar os dados da subscription no FaunaDB
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscriptionId,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await fauna.query(
+    q.Create(q.Collection("subscriptions"), { data: subscription })
+  );
 }
