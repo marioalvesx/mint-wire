c2442389229da49bfdcecdb11972fff7fc183491
Mario Alves <marioalvesneto@hotmail.com>
2025-06-26 23:42:39 -0300

    fix: Apply submit via Github - No database save

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,36 +10,45 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      authorization: { params: { scope: "read:user" } },
+      authorization: { params: { scope: "read:user, user:email" } },
     }),
   ],
 
-  jwt: {
-    secret: process.env.SIGNING_KEY,
-  },
+  // jwt: {
+  //   secret: process.env.SIGNING_KEY,
+  // },
 
-  callbacks: {
-    async signIn({ user, account, profile, credentials }) {
-      const isAllowedToSignIn = true;
-      const { email } = user;
+  // callbacks: {
+  //   async session(session) {
+  //     const userActiveSubscription = await fauna.query(
+  //       q.Get(q.Match(q.Index("subscription_by_user_ref")))
+  //     );
 
-      try {
-        await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(q.Index("user_by_email"), q.Casefold(user.email!))
-              )
-            ),
-            q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email!)))
-          )
-        );
-      } catch (error) {
-        return false;
-      }
+  //     return session;
+  //   },
+  //   async signIn({ user, account, profile, credentials }) {
+  //     const isAllowedToSignIn = true;
+  //     const { email } = user;
 
-      return isAllowedToSignIn ? true : false;
-    },
-  },
+  //     try {
+  //       // await fauna.query(
+  //       //   q.If(
+  //       //     q.Not(
+  //       //       q.Exists(
+  //       //         q.Match(q.Index("user_by_email"), q.Casefold(user.email!))
+  //       //       )
+  //       //     ),
+  //       //     q.Create(q.Collection("users"), { data: { email } }),
+  //       //     q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email!)))
+  //       //   )
+  //       // );
+  //     } catch (error) {
+  //       console.log(error);
+
+  //       return false;
+  //     }
+
+  //     return isAllowedToSignIn ? true : false;
+  //   },
+  // },
 });
