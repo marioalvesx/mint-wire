8f3fb6e6461b51f9c96769ff06d6ef9b5e4f4cdd
Mario Alves <marioalvesneto@hotmail.com>
2025-08-23 23:29:44 -0300

    fix: Change baseURL for production

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,6 @@ import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-import { fauna } from "../../../services/fauna";
-
 export default NextAuth({
   providers: [
     GithubProvider({
